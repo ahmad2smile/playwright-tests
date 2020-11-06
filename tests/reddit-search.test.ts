c74c626b8d07f473e5d0616402bca36ef79a17c2
ahmad <ahmad.shafiq@celus.io>
2020-11-06 19:51:21 +0000

    2 Fixed 1 Failed browser tests

--- tests/reddit-search.test.ts
+++ tests/reddit-search.test.ts
@@ -3,22 +3,33 @@ import { it, expect, describe } from "@playwright/test";
 describe("open a Dota 2 sub-reddit with Reddit Search", () => {
     const screen = "https://www.reddit.com/";
 
+    // NOTE: Fails on Firefox because of some shadow-drop by Reddit
     it("simple search", async ({ page }) => {
         await page.goto(screen);
         
+        // NOTE: Normal mode is css selector
         await page.type(
             '#header-search-bar',
             'Dota 2'
         );
-    
+        // NOTE: Dropdown is set to appear onClick event
+        await page.click(
+            '#header-search-bar'
+        );
+        await page.waitForSelector('#SearchDropdownContent > a');
         await page.click('#SearchDropdownContent > a');
 
+        // NTOE: "double qoute string" is a string lookup on page
+        // NOTE: Example of page loading wait indicator (indirect)
         await page.waitForSelector('"pinned by moderators"');
+
+        // NOTE: A more direct example of page load/SPA App load complete.
+        // await page.waitForLoadState('networkidle');
     
         await page.screenshot({ path: 'dota-2-test.png', type: 'png' });
         
         const subRedditName = await page.innerText('h1');
 
-      expect(subRedditName).toBe("Dota 2 on Reddit");
+        expect(subRedditName).toBe("Dota 2 on Reddit");
     });
 })
